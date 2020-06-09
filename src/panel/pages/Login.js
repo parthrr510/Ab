e8e5293e42fccad17d01bf6ca10154a1c4736ab8
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-06-09 20:59:01 +0530

    added routers

---
+++ src/panel/pages/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>To the Login Page</h1>
+      </div>
+    );
+  }
+}
