69fb97dd614f08c4141ee29dd06c7e901a26e656
Shouraya <shourayagoyal2406@gmail.com>
2020-06-09 14:52:32 +0530

    added scroll icon and it's functionality

--- src/landing page/Landing.js
+++ src/landing page/Landing.js
@@ -5,6 +5,7 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import logo from "./msc_logo.png";
 import Animation from "./Animation.js";
+import scroll from "./scroll.png";
 
 export default function Landing() {
   const [show, setShow] = useState(false);
@@ -37,6 +38,37 @@ export default function Landing() {
                 Get Started
               </Button>{" "}
             </div>
+            <img
+              id="scroll_icon"
+              src={scroll}
+              onClick={handleShow}
+              style={{ height: "80px" }}
+            />
+            <Modal show={show} onHide={handleClose} className="rulebook">
+              <Modal.Header closeButton>
+                <Modal.Title>RuleBook</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  XYZ
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         </div>
       </section>
