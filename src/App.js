9ae6cb9e82e3f2c688974eb5a7693c7667fdb183
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-06-12 21:33:58 +0530

    initial setup for private routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LeaderBoard from "./panel/pages/LeaderBoard";
-import Updates from "./panel/pages/Updates";
-import Trading from "./panel/pages/Trading";
-import TradeRulebook from "./panel/pages/TradeRulebook";
-import Discord from "./panel/pages/Discord";
-import Landing from "./landing page/Landing";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import SideNav from "./panel/SideNav";
+import Routes from "./routes";
+import History from "./services/History";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/panel" component={SideNav} />
-            <Route exact path="/leaderboard" component={LeaderBoard} />
-            <Route exact path="/updtes" component={Updates} />
-            <Route exact path="/trading" component={Trading} />
-            <Route exact path="/traderulebook" component={TradeRulebook} />
-            <Route exact path="/discord" component={Discord} />
-          </Switch>
+        <Router history={History}>
+          <Routes />
         </Router>
       </div>
     );
