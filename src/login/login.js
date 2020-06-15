1b99bc046b6835e8d438660445ad77c2fb7fb2a6
Shouraya <shourayagoyal2406@gmail.com>
2020-06-15 18:28:51 +0530

    added styling in input box

--- src/login/login.js
+++ src/login/login.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import {
-  Row,
-  Col,
-  Nav,
-  Navbar,
-  NavDropdown,
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Row, Col, Nav, Navbar, Form, Button } from "react-bootstrap";
 import Media from "react-bootstrap/Media";
 import { Link } from "react-router-dom";
 import logo from "../panel/msc_logo.png";
 import "./login.css";
-// import bg from "./bg.jpg";
 
 class Login extends Component {
   render() {
@@ -40,23 +31,25 @@ class Login extends Component {
           <Col md={2}></Col>
           <Col md={8}>
             <Form id="form">
-              <Form.Group controlId="formBasicEmail">
+              <h3>Login</h3>
+              <hr></hr>
+              <Form.Group className="form_field" controlId="formBasicEmail">
                 <Form.Label className="form_inp">Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group className="form_field" controlId="formBasicPassword">
                 <Form.Label className="form_inp">Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
+              <Form.Group className="form_field" controlId="formBasicCheckbox">
                 <Form.Check
                   className="form_inp"
                   type="checkbox"
                   label="Keep me signed in"
                 />
               </Form.Group>
-              <Button className="form_inp" variant="primary" type="submit">
+              <Button className="form_inp sub_btn" type="submit">
                 Submit
               </Button>
             </Form>
