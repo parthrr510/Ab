92a52cda164e9741de3d6a12d2fde719c9cf4ec5
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-06-25 12:08:46 +0530

    Daily depriciation function and route

--- panel/api/urls.py
+++ panel/api/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path("resources/", views.ResourceView.as_view(), name="resources"),
     path("leaderboard/", views.LeaderBoard.as_view(), name="leaderboard"),
     path("trade/", views.Trade.as_view(), name="trade"),
+    path(
+        "depriciate/msc/69/420/69/gormint-aunty/the-night-we-met/",
+        views.Depreciate.as_view(),
+        name="depriciate",
+    ),
 ]
