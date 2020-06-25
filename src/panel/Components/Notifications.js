3a88c62c101f3e9d8dd3cc1a8032a0f6953a8810
Shouraya <shourayagoyal2406@gmail.com>
2020-06-25 20:22:03 +0530

    removed image in notification

--- src/panel/Components/Notifications.js
+++ src/panel/Components/Notifications.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
-
 import { MdNotificationsActive } from "react-icons/md";
-
 import Avatar from "@material-ui/core/Avatar";
-
 import team from "../team.PNG";
 import "./notification.css";
 
@@ -21,26 +18,14 @@ const Notifications = ({ show, handleClose }) => {
         <Modal.Body>
           <ul id="root">
             <li className="item">
-              <Avatar>
-                <img src={team} />
-              </Avatar>
-
               <p className="text">Any Random Notification</p>
             </li>
             <hr className="line" />
             <li className="item">
-              <Avatar>
-                <img src={team} />
-              </Avatar>
-
               <p className="text">Any Random Notification</p>
             </li>
             <hr className="line" />
             <li className="item">
-              <Avatar>
-                <img src={team} />
-              </Avatar>
-
               <p className="text">Any Random Notification</p>
             </li>
           </ul>
