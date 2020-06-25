605818919497a984797b7ae1d8c6a7c603f56d20
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-06-25 18:37:11 +0530

    notification comp

--- src/panel/Components/Notifications.js
+++ src/panel/Components/Notifications.js
@@ -1,75 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import { MdNotificationsActive } from "react-icons/md";
-import { makeStyles } from "@material-ui/core/styles";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import Typography from "@material-ui/core/Typography";
-import team from "../team.PNG";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
+import { MdNotificationsActive } from "react-icons/md";
+
+import Avatar from "@material-ui/core/Avatar";
+
+import team from "../team.PNG";
+import "./notification.css";
+
 const Notifications = ({ show, handleClose }) => {
-  const classes = useStyles();
   return (
     <div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
+      <Modal show={show} onHide={handleClose} style={{ borderRadius: "50px" }}>
+        <Modal.Header closeButton style={{ backgroundColor: "#2e3035" }}>
+          <Modal.Title style={{ color: "#fff" }}>
             <MdNotificationsActive /> Notifications
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <List className={classes.root}>
-            <ListItem style={{ height: "20px" }}>
-              <ListItemAvatar style={{ height: "20px" }}>
-                <Avatar>
-                  <img src={team} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="Random Notification"
-                secondary="Jan 9, 2014"
-                style={{ margin: "0", height: "15px" }}
-              />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <img src={team} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Work" secondary="Jan 7, 2014" />
-            </ListItem>
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <img src={team} />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary="Vacation" secondary="July 20, 2014" />
-            </ListItem>
-          </List>
+          <ul className="root">
+            <li className="item" style={{ display: "flex" }}>
+              <Avatar>
+                <img src={team} />
+              </Avatar>
+
+              <p className="text">Any Random Notification</p>
+            </li>
+            <hr className="line" />
+            <li className="item" style={{ display: "flex" }}>
+              <Avatar>
+                <img src={team} />
+              </Avatar>
+
+              <p className="text">Any Random Notification</p>
+            </li>
+            <hr className="line" />
+            <li className="item" style={{ display: "flex" }}>
+              <Avatar>
+                <img src={team} />
+              </Avatar>
+
+              <p className="text">Any Random Notification</p>
+            </li>
+          </ul>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </div>
   );
