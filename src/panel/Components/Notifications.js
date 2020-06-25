b823ad50fb7581c6982aa37b63648e7c7aa46791
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-06-25 14:39:55 +0530

    changes

--- src/panel/Components/Notifications.js
+++ src/panel/Components/Notifications.js
@@ -2,15 +2,66 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { MdNotificationsActive } from "react-icons/md";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import team from "../team.PNG";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 const Notifications = ({ show, handleClose }) => {
+  const classes = useStyles();
   return (
     <div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>
+            <MdNotificationsActive /> Notifications
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <List className={classes.root}>
+            <ListItem style={{ height: "20px" }}>
+              <ListItemAvatar style={{ height: "20px" }}>
+                <Avatar>
+                  <img src={team} />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Random Notification"
+                secondary="Jan 9, 2014"
+                style={{ margin: "0", height: "15px" }}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <img src={team} />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Work" secondary="Jan 7, 2014" />
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <img src={team} />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Vacation" secondary="July 20, 2014" />
+            </ListItem>
+          </List>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
