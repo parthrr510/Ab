6ca489152d7ee68b91fcac05d75a76a0bba824c2
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-06-28 17:25:06 +0530

    Notification model

--- panel/admin.py
+++ panel/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Resource, Trade, Question, Submission
+from .models import Resource, Trade, Question, Submission, Notification
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Resource)
 admin.site.register(Trade)
 admin.site.register(Question)
 admin.site.register(Submission)
+admin.site.register(Notification)
