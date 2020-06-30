636a5998ea90ba502537cfd2d5c31d3a94752b54
Dv2204 <Divyanshujindal990@gmail.com>
2020-07-01 01:09:15 +0530

    Completed Update component for navbar

--- src/panel/pages/Updates.js
+++ src/panel/pages/Updates.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { Row, Col, Nav } from "react-bootstrap";
+import "./update.css";
 
 const Updates = () => {
   return (
-    <div>
-      <h1>Updates</h1>
+    <div className="update">
+      <Row>
+        <div className="update-head">UPDATES</div>
+      </Row>
+      <Row >
+        <Col sm={1}></Col>
+        <Col sm={10} className="card">
+        <Row>
+          <div id="updates">UPDATE</div>
+        </Row>
+        <Row>
+          <div id="question">QUESTION</div>
+        </Row>
+        <Row>
+          <div id="image">Upload IMAGE Here</div>
+        </Row>
+        <Row>
+          <div id="pdf">Upload PDF here</div>
+        </Row>
+        <Row>
+          <Col sm={6} style={{paddingTop:"1rem", paddingBottom:"5rem"}}>
+          <input type="text" id="input-box"></input>
+          </Col>
+          <Col sm={6} style={{paddingTop:"1rem"}}>
+          <input type="submit" id="submit-btn"></input>
+          </Col>
+          
+        </Row>
+        </Col>
+        <Col sm={1}></Col>
+
+      </Row>
     </div>
   );
 };
