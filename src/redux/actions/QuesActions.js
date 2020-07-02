20bd04e79077011e53993a058b0160be77bc2498
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-02 12:43:01 +0530

    setup for ques

---
+++ src/redux/actions/QuesActions.js
@@ -0,0 +1,28 @@
+import { GET_QUESTIONS, GET_ERROR } from "../types";
+import axios from "axios";
+
+export const getNotifications = () => async (dispatch) => {
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTkzNjI4MjQyLCJqdGkiOiIxODgzYmYzYzU4MWM0YmVjOWNmZWRiZDg4ODFmOTNiMiIsInVzZXJfaWQiOiI2Zjg4ZDhiOC1kNTQ5LTQ3NzgtYjI4My0yZTAzZTYzOGQzY2IifQ.ch3BDc_Rlc9QvWmz8g0mIxd5YgiZm5xhQVOnpOUmJM4",
+    },
+  };
+
+  try {
+    const res = await axios.get(
+      "http://127.0.0.1:8000/api/panel/questions/",
+      config
+    );
+    console.log(res.data);
+    dispatch({
+      type: GET_QUESTIONS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: GET_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
