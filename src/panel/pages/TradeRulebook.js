1876f1d4b9bee6a69a824ce01061bcf84b0b49b1
Dv2204 <Divyanshujindal990@gmail.com>
2020-07-03 18:10:12 +0530

    added rulebook pdf to panel

--- src/panel/pages/TradeRulebook.js
+++ src/panel/pages/TradeRulebook.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "react-bootstrap/Modal";
-
+import Iframe from "react-iframe";
+import Pdf from "../../landing page/Rule_Book_Final.pdf";
 const TradeRulebook = ({ showBook, handleCloseBook }) => {
   return (
     <div>
@@ -14,15 +15,11 @@ const TradeRulebook = ({ showBook, handleCloseBook }) => {
           <Modal.Title>Trade Rulebook</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+        <Iframe
+                    src={Pdf}
+                    className="rulebook"
+                    frameborder="0"
+                  ></Iframe>
         </Modal.Body>
       </Modal>
     </div>
